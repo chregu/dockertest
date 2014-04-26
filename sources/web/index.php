52a5df0aa3536def226a6fb04293fb8d88fddbd1
Christian Stocker <me@chregu.tv>
2014-04-26 18:48:24 +0200

    fix a typo

--- sources/web/index.php
+++ sources/web/index.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/hello/{name}', function ($name) use ($app) {
-    return 'This is form Silex. Hello '.$app->escape($name);
+    return 'This is from Silex. Hello '.$app->escape($name);
 });
 
 $app->run();
